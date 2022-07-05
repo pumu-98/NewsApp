cdf72cd686e548d71c0e0bc3ab7282d94c93d9a1
Udara Jayasekara <it19113296@my.sliit.lk>
2022-07-05 11:23:20 +0530

    full code

---
+++ flutter_application_exam/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Asus\\Documents\\Flutter_VS\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Asus\\Documents\\Flutter_VS\\flutter"
+  "PROJECT_DIR=C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam"
+  "FLUTTER_ROOT=C:\\Users\\Asus\\Documents\\Flutter_VS\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam"
+  "FLUTTER_TARGET=C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Asus\\Documents\\4th year\\ctseass\\flutter_application_exam\\.dart_tool\\package_config.json"
+)
